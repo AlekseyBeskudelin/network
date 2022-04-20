bc98946b1433eceae77a1aad5f5c4e37d1b3484c
Aleksey Beskudelin <anbeskudelin@gmail.com>
2022-04-20 17:33:30 +0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main (String [] args){
+        int port = 8085;
+
+    }
+}
---
+++ src/main/java/SocketApp.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SocketApp {
+    public static void main (String [] args) throws IOException {
+        int port = 8085;
+        String host = "localhost";
+        try(Socket clientSocket = new Socket(host, port);
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))){
+                out.println("Netology");
+                String resp = in.readLine();
+                System.out.println(resp);
+        }
+
+
+    }
+}
